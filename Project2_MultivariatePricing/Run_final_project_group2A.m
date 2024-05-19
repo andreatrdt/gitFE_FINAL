34900f4cd848b2f2550c11114b1e0d08515e5fc2
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-05-19 17:12:08 +0200

    new start

--- Project2_MultivariatePricing/Run_final_project_group2A.m
+++ Project2_MultivariatePricing/Run_final_project_group2A.m
@@ -57,50 +57,6 @@ end
 %% Creation of the constraints for the simulations
 
 %% calibration 
-alpha = 1/2; %(NIG model)
-
-M = 15;
-dz = 0.0025;
-t = 1;
-
-data = data_EU;
-
-idx=1;
-
-date = data.datesExpiry(idx);
-
-[F_vector, G_vector , discount_1y ] = forward_prices(data, date);
-
-F_0 = F_vector(1,:);
-
-% compute the log moneyess from the strikes
-log_moneyness = log( F_0 ./ data.strikes(idx).value);
-
-% create the distance function to minimize
-dist = @(x) abs(sum((E3_callPriceLewis(discount_1y, F_0, alpha, log_moneyness, x(1), x(2), x(3), t, 2) - data.callAsk(idx).prices)));
-
-% create the constraint
-const = @(x) constraint(x, alpha);
-
-% calibrate the model using fmincon
-% initial guess
-x0 = [0.1, 1, 3];
-
-A = [
-    -1, 0, 0;
-    0, -1, 0;
-];
-b = [
-    0;
-    0;
-];
-
-% calibration
-options = optimoptions('fmincon', 'Display', 'iter');
-
-[x, fval] = fmincon(dist, x0, A, b, [], [], [], [], const, options)
-
-
 
 % % General function handles for the writing
 
--- Project2_MultivariatePricing/calibration/constraint.m
+++
@@ -1,7 +0,0 @@
-function [c, ceq] = constraint(x, alpha)
-    % function that represents the constraint in the Least Squares Calibration
-    %p1=sigma
-    %p2 k, p3 eta
-    c = - (1-alpha) / (x(2)*x(1)^2) - x(3);
-    ceq = [];
-end
