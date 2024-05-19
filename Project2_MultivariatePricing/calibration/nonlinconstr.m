d29b23e7e4fab8deb398a397d601ec1cfe0207f3
MarcoMaspes <marco.maspes00@gmail.com>
2024-05-19 15:04:41 +0200

    Update notation

--- Project2_MultivariatePricing/calibration/nonlinconstr.m
+++ Project2_MultivariatePricing/calibration/nonlinconstr.m
@@ -16,11 +16,11 @@ function [c, ceq] = nonlinconstr(a_1, a_2, beta_1, nu_1, gamma_1, beta_2, nu_2, 
     %% Constraints on conditions 10
     
     constraint_1 = sigma2_i(gamma_1, a_1)/(k_i(nu_1) * (theta_i(beta_1, a_1)^2)) - gamma_z/(nu_z * beta_z^2);
-    constraint_2 = sigma2_i(gamma_2, a_2)/(k_i(nu_2) * (theta_i(beta_2, a_2)^2)) - gamma_z/(nu_z * beta_z^2);
+%     constraint_2 = sigma2_i(gamma_2, a_2)/(k_i(nu_2) * (theta_i(beta_2, a_2)^2)) - gamma_z/(nu_z * beta_z^2);
 
     %% Creation of the final vectors on inequalities
 
     c = [];
-    ceq = [constraint_1; constraint_2];
+    ceq = constraint_1;
 
 end
