37ae307b1bef5a2e24762b4a8f69f9963a5a6e78
MarcoMaspes <marco.maspes00@gmail.com>
2024-05-20 23:21:58 +0200

    Various
    Modification of the calibration paper (dead end)
    
    Change in the nonlinear constraints

--- Project2_MultivariatePricing/calibration/nonlinconstr.m
+++ Project2_MultivariatePricing/calibration/nonlinconstr.m
@@ -17,13 +17,9 @@ function [c, ceq] = nonlinconstr(x, rho_mkt, eps_th)
 
     % Constraint to create the entire equality given on the final
     % parameters
-
-    ceq = [ x(3)^2/(x(1)*x(2)^2) - x(6)^2/(x(4)*x(5)^2); ...
-        x(3^2)/(x(1)*x(2)^2) - x(9)^2/(x(7)*x(8)^2); ...
-        x(1)*x(2)-x(7)*x(10)*x(8);
-        x(4)*x(5)-x(7)*x(11)*x(8);
-        x(1)*x(3)^2-x(7)*x(10)^2*x(9)^2;
-        x(4)*x(6)^2-x(7)*x(11)^2*x(9)^2];
+    ceq = [];
+%     ceq = [ x(1)*x(2)-x(7)*x(10)*x(8);
+%         x(1)*x(3)^2-x(7)*x(10)^2*x(9)^2];
 
     % ceq = [ x(3)^2/(x(1)*x(2)^2) - x(6)^2/(x(4)*x(5)^2)];
     %% Constraints on the inequalities
