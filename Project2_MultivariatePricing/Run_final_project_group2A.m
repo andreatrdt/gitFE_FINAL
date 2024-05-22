c9345f71ef356839968e3774747e21a7d4c32db0
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-05-22 16:45:42 +0200

    Update Run_final_project_group2A.m

--- Project2_MultivariatePricing/Run_final_project_group2A.m
+++ Project2_MultivariatePricing/Run_final_project_group2A.m
@@ -54,89 +54,63 @@ data_USA = OTM_preprocessing(data_USA);
 % clean dataset from too far from the ATM point prices
 
 data_EU = dataset_preprocessing(data_EU, F0_EU, B_bar_EU, date_settlement, 0);
-data_USA = dataset_preprocessing(data_USA, F0_USA, B_bar_USA, date_settlement, 1);
-
-a = 1
+data_USA = dataset_preprocessing(data_USA, F0_USA, B_bar_USA, date_settlement, 0);
 
 
-
-
-
-
-
-
-
-
-%% POINT 6: Model calibration
-
-% Computation of the correlation of the market to impose the non linear
-% constraint in the calibration
-
-rho_mkt = zeros(length(data_EU.datesExpiry), 1);
-
-for i = 1:length(data_EU.datesExpiry)
-
-    date = data_EU.datesExpiry(i);
-    rho_mkt(i) = compute_corr_coeff(SP500_EUR500, date_settlement, date); 
-end
-
-
-
-
-%% Joint calibration
+%% calibration
 
 % Quantities of interest
 
 alpha = 1/2;       % NIG model
 idx = 1;
 
+
 % EU:
 data = data_EU;
-date = data.datesExpiry(idx);
-
-% compute the forward in 0:
-[F_0_EU, ~ , discount_at_expiry_EU] = forward_prices(data, date, 1);
 
 % compute the log moneyess from the strikes
-log_moneyness = log(F_0_EU(1,:) ./ data.strikes(idx).value);
+log_moneyness = log(F_0_EU(1) ./ data.strikes(idx).value);
 
 % time to maturity
 t = yearfrac(date_settlement,dates_EU(idx),conv_ACT365);
 
 % create a function that the prices of the call options given the strikes
-prices_EU = @(p) callIntegral(discount_at_expiry_EU, F_0_EU(1,:), p(1), p(2), p(3), t, log_moneyness); % CORRECT and use integral
-
-% compute the implied volatilities:
-volatility_EU = @(p) blkimpv(F_0_EU(1,:), data.strikes(idx).value, -log(discount_at_expiry_EU)/t, t, prices_EU(p));
+prices_EU = @(p) callIntegral(discount_at_expiry_EU(idx), F_0_EU(1), p(1), p(2), p(3), t, log_moneyness); % CORRECT and use integral
 
 
 % USA:
 data = data_USA;
-date = data.datesExpiry(idx);
-
-% compute the forward in 0:
-[F_0_USA, ~ , discount_at_expiry_USA] = forward_prices(data, date, 1);
 
 % compute the log moneyess from the strikes
-log_moneyness = log(F_0_USA(1,:) ./ data.strikes(idx).value);
+log_moneyness = log(F_0_USA(1) ./ data.strikes(idx).value);
 
 % time to maturity
 t = yearfrac(date_settlement,dates_USA(idx),conv_ACT365);
 
 % create a function that the prices of the call options given the strikes
-prices_USA = @(p) callIntegral(discount_at_expiry_USA, F_0_USA(1,:), p(1), p(2), p(3), t, log_moneyness);
+prices_USA = @(p) callIntegral(discount_at_expiry_USA(idx), F_0_USA(1), p(1), p(2), p(3), t, log_moneyness);
 
-% compute the implied volatilities:
-volatility_USA = @(p) blkimpv(F_0_USA(1,:), data.strikes(idx).value, -log(discount_at_expiry_USA)/t, t, prices_USA(p));
 
 
 % create the distance function to minimize
 % dist = @(p_EU,p_USA) 1/length(data_EU.callAsk(idx).impvol)*sum((volatility_EU(p_EU)' - data_EU.callAsk(idx).impvol).^2) + 1/length(data_USA.callAsk(idx).impvol)*sum((volatility_USA(p_USA)' - data_USA.callAsk(idx).impvol).^2);
 mean_call_price_EU = (data_EU.callAsk(idx).prices+data_EU.callBid(idx).prices)/2;
+
+idx_EU = find(mean_call_price_EU >= 0);
+
+mean_call_price_EU = mean_call_price_EU(idx_EU);
+
 mean_call_price_USA = (data_USA.callAsk(idx).prices+data_USA.callBid(idx).prices)/2;
 
-weights_EU = data_EU.Volume_call(idx).volume./sum(data_EU.Volume_call(idx).volume);
-weights_USA = data_USA.Volume_call(idx).volume./sum(data_USA.Volume_call(idx).volume);
+idx_USA = find(mean_call_price_USA >= 0);
+
+mean_call_price_USA = mean_call_price_USA(idx_USA);
+
+% weights
+
+
+
+
 
 dist = @(p_EU,p_USA) 1/length(data_EU.callAsk(idx).prices)*sum(weights_EU' .* (prices_EU(p_EU) - mean_call_price_EU).^2) + ...
     1/length(data_USA.callAsk(idx).prices)*sum(weights_USA' .* (prices_USA(p_USA) - mean_call_price_USA).^2);
@@ -171,16 +145,3 @@ ub = [];
 options = optimset('Display', 'iter');
 
 x = fmincon(@(x) dist([x(1) x(2) x(3)],[x(4) x(5) x(6)]), x0, A, b, Aeq, beq, lb, ub, @(x) nonlinconstr(x, rho_mkt(1), esp_thr), options);
-
-% 
-%     0.0094
-%    83.6219
-%     0.1058
-%     0.0590
-%    10.8153
-%     0.1121
-%    16.7909
-%    56.5838
-%     3.0280
-%     0.0008
-%     0.3834
