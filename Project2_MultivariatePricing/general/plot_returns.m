e64144e287e6117469f44f116c0fa7a1b020a137
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-05-23 15:40:17 +0200

    easy

--- Project2_MultivariatePricing/general/plot_returns.m
+++ Project2_MultivariatePricing/general/plot_returns.m
@@ -6,22 +6,26 @@ function plot_returns(data,date_settlement)
 % OUTPUTS:
 % plot the returns of the options
 
+    % Load the data
     returns_EU = flip(data.Daily(:,1));
 
     returns_USA = flip(data.Daily(:,2));
 
+    % Create the dates
     days = [date_settlement : date_settlement+length(returns_USA)-1];
 
-
+    % Plot the returns
     figure
     subplot(3,1,1)
     plot(days,returns_EU)
     title('EU returns')
     datetick('x','dd-mmm-yyyy','keepticks')
+
     subplot(3,1,2)
     plot(days,returns_USA,"Color","r")
     title('USA returns')
     datetick('x','dd-mmm-yyyy','keepticks')
+    
     subplot(3,1,3)
     plot(days,returns_EU)
     hold on
@@ -31,37 +35,37 @@ function plot_returns(data,date_settlement)
     datetick('x','dd-mmm-yyyy','keepticks')
     hold off
 
-    filename_EU = 'eurostocks.xlsx';
+    % filename_EU = 'eurostocks.xlsx';
 
-    filename_USA = 'SP500.xls';
+    % filename_USA = 'SP500.xls';
 
-    returns_EU = readtable(filename_EU,Range="C6:C229");
+    % returns_EU = readtable(filename_EU,Range="C6:C229");
 
-    returns_EU = table2array(returns_EU);
+    % returns_EU = table2array(returns_EU);
 
-    dates_EU = readtable(filename_EU,RAnge = "B6:B229");
+    % dates_EU = readtable(filename_EU,RAnge = "B6:B229");
 
-    dates_EU = table2array(dates_EU);
+    % dates_EU = table2array(dates_EU);
 
   
-    returns_USA = readtable(filename_USA,Range = "B12:B1317");
+    % returns_USA = readtable(filename_USA,Range = "B12:B1317");
 
-    returns_USA = table2array(returns_USA);
+    % returns_USA = table2array(returns_USA);
 
-    dates_USA = readtable(filename_USA,Range = "A12:A1317");
+    % dates_USA = readtable(filename_USA,Range = "A12:A1317");
 
-    dates_USA = table2array(dates_USA);
+    % dates_USA = table2array(dates_USA);
 
     
-    figure
-    subplot(2,1,1)
-    plot(datenum(dates_EU),returns_EU)
-    title('EU real returns')
-    datetick('x','dd-mmm-yyyy','keepticks');
-    subplot(2,1,2)
-    plot(datenum(dates_USA),returns_USA,"Color","r")
-    title('USA real returns')
-    datetick('x','dd-mmm-yyyy','keepticks');
+    % figure
+    % subplot(2,1,1)
+    % plot(datenum(dates_EU),returns_EU)
+    % title('EU real returns')
+    % datetick('x','dd-mmm-yyyy','keepticks');
+    % subplot(2,1,2)
+    % plot(datenum(dates_USA),returns_USA,"Color","r")
+    % title('USA real returns')
+    % datetick('x','dd-mmm-yyyy','keepticks');
     
 
 
