1918438c811b59c86f2e96261ef077cf6daec646
MarcoMaspes <marco.maspes00@gmail.com>
2024-05-23 17:12:12 +0200

    Idk
    Modifiche per ridurre i parametri

--- Project2_MultivariatePricing/Run_final_project_group2A.m
+++ Project2_MultivariatePricing/Run_final_project_group2A.m
@@ -66,13 +66,16 @@ data_USA = dataset_preprocessing(data_USA, F0_USA, B_bar_USA, date_settlement, 0
 
 %% Calibration of the model parameters
 
-% x0 = [5 0.1 0.1 5 0.1 0.1];
-
 % % Quantities of interest
 x0 = [10 2 0.5 10 2 0.5];
 
 % Linear inequality constraints 
-A = []; b = [];
+A = [-1 0 0 0 0 0;
+    0 0 -1 0 0 0;
+    0 0 0 -1 0 0;
+    0 0 0 0 0 -1];
+
+b = [0; 0; 0; 0];
 
 % Linear equality constraints
 Aeq = []; beq = [];
@@ -88,10 +91,10 @@ options = optimset('Display', 'iter');
 params_marginals = fmincon(@(params) new_calibration(params, data_EU, data_USA, ...
     F0_EU, B_bar_EU, F0_USA, B_bar_USA, date_settlement), x0, A, b, Aeq, beq, lb, ub, @(params) nonlinconstr(params), options)
 
-% log_moneyness = log(F0_EU(2) ./ data_EU.strikes(2).value);
-% TTM = yearfrac(date_settlement, datenum(data_EU.datesExpiry(2)), conv_ACT365);
-% prices = callPriceLewis(B_bar_EU(2), F0_EU(2), log_moneyness, params_marginals(6), params_marginals(4), params_marginals(5), TTM, 15, 0.0025)
-% mean_call_price = (data_EU.callAsk(2).prices + data_EU.callBid(2).prices)/2
+log_moneyness = log(F0_EU(1) ./ data_EU.strikes(1).value);
+TTM = yearfrac(date_settlement, datenum(data_EU.datesExpiry(1)), conv_ACT365);
+prices = callPriceLewis(B_bar_EU(1), F0_EU(1), log_moneyness, params_marginals(6), params_marginals(4), params_marginals(5), TTM, 15, 0.0025)
+mean_call_price = (data_EU.callAsk(1).prices + data_EU.callBid(1).prices)/2
 
 %% 2nd Calibration over the rho
 
--- Project2_MultivariatePricing/calibration/dataset_preprocessing.m
+++ Project2_MultivariatePricing/calibration/dataset_preprocessing.m
@@ -50,8 +50,8 @@ function [dataset] = dataset_preprocessing(dataset, F0, B0, date_settlement, fla
         %% Computation of the delta
 %         [delta_call, delta_put] = blsdelta(F0(ii)*B0(ii), strikes, interest_rate, TTM, [impvol_put_i impvol_call_i]);
 
-        [delta_call, ~] = blsdelta(spot_ATM, strikes(idx_call_OTM), interest_rate, TTM, impvol_call_i);
-        [~, delta_put] = blsdelta(spot_ATM, strikes(idx_put_OTM), interest_rate, TTM, impvol_put_i);
+        [delta_call, ~] = blsdelta(F0(ii) * B0(ii), strikes(idx_call_OTM), interest_rate, TTM, impvol_call_i);
+        [~, delta_put] = blsdelta(F0(ii) * B0(ii), strikes(idx_put_OTM), interest_rate, TTM, impvol_put_i);
 
         %% Restructuring of the dataset
 
@@ -67,7 +67,6 @@ function [dataset] = dataset_preprocessing(dataset, F0, B0, date_settlement, fla
         indicator_90 = delta_call <= 0.9;
         indicator = indicator_10 .* indicator_90;
 
-        idx_call = find(indicator > 0);
         idx_call = find(indicator > 0) + length(delta_put);
 
         % Cut of the structs
