30fd55f5db5fb0f96108774a2ca12071fc3dddd4
MarcoMaspes <marco.maspes00@gmail.com>
2024-05-23 14:20:38 +0200

    Various
    Modification of the FFT

--- Project2_MultivariatePricing/Run_final_project_group2A.m
+++ Project2_MultivariatePricing/Run_final_project_group2A.m
@@ -63,34 +63,41 @@ data_USA = dataset_preprocessing(data_USA, F0_USA, B_bar_USA, date_settlement, 0
 
 %% Calibration of the model parameters
 
-load('params.mat')
-
-x0 = [20 0.2 0.1 30 0.5 0.3];
-
-% % Quantities of interest
-% x0 = [10 2 0.5 10 2 0.5];
-
-% Linear inequality constraints 
-A = []; b = [];
-
-% Linear equality constraints
-Aeq = []; beq = [];
-
-% Lower and upper bounds 
-lb = [0; -Inf; 0; 0; -Inf; 0];
-ub = [];
-
-% Options for the visualization
-options = optimset('Display', 'iter');
-
-% Calibration
-params = fmincon(@(params) new_calibration(params, data_EU, data_USA, ...
-    F0_EU, B_bar_EU, F0_USA, B_bar_USA, date_settlement), x0, A, b, Aeq, beq, lb, ub, @(params) nonlinconstr(params), options)
+% load('params.mat')
+% 
+% x0 = [20 0.2 0.1 30 0.5 0.3];
+% 
+% % % Quantities of interest
+% % x0 = [10 2 0.5 10 2 0.5];
+% 
+% % Linear inequality constraints 
+% A = []; b = [];
+% 
+% % Linear equality constraints
+% Aeq = []; beq = [];
+% 
+% % Lower and upper bounds 
+% lb = [0; -Inf; 0; 0; -Inf; 0];
+% ub = [];
+% 
+% % Options for the visualization
+% options = optimset('Display', 'iter');
+% 
+% % Calibration
+% params = fmincon(@(params) new_calibration(params, data_EU, data_USA, ...
+%     F0_EU, B_bar_EU, F0_USA, B_bar_USA, date_settlement), x0, A, b, Aeq, beq, lb, ub, @(params) nonlinconstr(params), options)
 
 % params(T=1) =   0.1300   19.3334    0.1200    0.0257   42.8258    0.1182
 % params(T=2) =    1.1832   10.0741    0.1473    0.3111   17.4346    0.1307
 % CAMBIO DELLA FUNZIONE OBJ (prima avevo dimenticato la radice, quindi era un MSE pesato)
 % params(T=3) =   1.0e+03 *    0.0000   -0.6944    0.0002    4.0417    0.0010    0.0042
+
+load('params_II.mat');
+
+log_moneyness = log(F0_EU(1) ./ data_EU.strikes(1).value);
+TTM = yearfrac(date_settlement, datenum(data_EU.datesExpiry(1)), conv_ACT365);
+prices = E3_callPriceLewis(B_bar_EU(1), F0_EU(1), 1/2, log_moneyness, params(6), params(4), params(5), TTM, 1, 19, 0.0048)
+mean_call_price = (data_EU.callAsk(1).prices + data_EU.callBid(1).prices)/2
 
 %% 2nd Calibration over the rho
 
---
+++ Project2_MultivariatePricing/calibration/callPriceLewis.m
@@ -0,0 +1,54 @@
+function price = callPriceLewis(B0, F0, log_moneyness, sigma, k, theta, TTM, M, dz)
+% Call price with Lewis formula
+%
+%INPUT
+% B0:                 [SCALAR] discount factor B(t0, t)
+% F0:                 [FORWARD] initial fwd price
+% log_moneyness:      [VECTOR] vector of values of moneyness
+% sigma:              [SCALAR] volatility of the model
+% k:                  [SCALAR] vol of vol of the model
+% theta:              [SCALAR] simile asimmetry of the model
+% TTM:                [SCALAR} time to maturity
+% M:                  parameter for FFT
+% dz:                 parameter of the moneyness grid
+% 
+%USES:
+% function E3_integralLewisFFT(M, dz, alpha, sigma, k, eta, timeToMaturity)
+
+    %% Introduction of the FFT parameters
+    
+    % Initial constraints for FFt parameters
+    N = 2^M;
+
+    dx = (2*pi)/(N*dz); xN = ((N-1)*dx)/2; x1 = -xN;
+    zN = ((N-1)*dz)/2; z1 = -zN;
+
+    %% Computation of the grid
+    X = (x1: dx: xN)';
+    Z = (z1: dz: zN)';
+
+    % Computation of prefactor
+    preFactor = dx * exp(-1i*x1.*Z);
+
+    % Computation of FFT input
+    xi = -X-1i/2;
+
+    %% Computation of the characteristic function
+    charFct = exp(TTM .* (1/k * (1 - sqrt(1 - 2i .* xi .* k .* eta + xi.^2 .* k .* sigma.^2))));
+
+    %% Computation of the integral
+    integrand = 1/(2*pi) * 1./(X.^2+1/4) .* charFct; 
+    j = (0:1:N-1)';
+    inputFFT = integrand .* exp(-1i*z1*dx.*j);
+    
+    % Computation of the integral
+    IntLewis = preFactor .* fft(inputFFT);
+
+    IntLewis = real(IntLewis);
+    IntLewis = interp1(Z, IntLewis, log_moneyness);
+
+    %% Computation of the Call Price through Lewis
+    
+    price = B0 * F0 * (1 - exp(-log_moneyness./2) .* IntLewis);
+    
+end % function E3_callPriceLewis
