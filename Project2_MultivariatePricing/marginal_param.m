ec6691f76c98389ed6bcf9b06e58f04d05e5038b
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-05-28 17:03:21 +0200

    Create marginal_param.m

---
+++ Project2_MultivariatePricing/marginal_param.m
@@ -0,0 +1,27 @@
+function [sol] = marginal_param(params,nu_z)
+
+kappa = params(1);
+theta = params(2);
+sigma = params(3);
+
+x = optimvar('x',5);
+eq1 = x(1) * x(2) - kappa * theta / nu_z == 0;
+eq2 = (x(3)*x(1))^2 - kappa * sigma^2 / nu_z == 0;
+eq3 = x(4) +x(1)*x(2) - theta == 0;
+eq4 = x(5)^2 + x(3)^2 * x(3)^2 - sigma^2 == 0;
+eq5 = x(3)^2/x(2)^2 - (sigma^2 * nu_z) / (kappa * theta^2) == 0;
+
+prob = eqnproblem;
+prob.Equations.eq1 = eq1;
+prob.Equations.eq2 = eq2;
+prob.Equations.eq3 = eq3;
+prob.Equations.eq4 = eq4;
+prob.Equations.eq5 = eq5;
+
+
+x0.x = 0.5 * ones(5,1);
+
+sol = solve(prob,x0);
+
+
+end
