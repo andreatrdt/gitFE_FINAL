7fd7d8d06c1ce784168bcb7e776b527bce90c28f
MarcoMaspes <marco.maspes00@gmail.com>
2024-05-28 15:34:14 +0200

    Update della yearfrac

--- Project2_MultivariatePricing/general/discount_factor.m
+++ Project2_MultivariatePricing/general/discount_factor.m
@@ -1,24 +1,27 @@
 function discount =  discount_factor(B_bar , data , date_settlement)
-%
-% INPUT
-% B_bar : scalar [1x1] : discount factor at time 0
-% data : structure : market data
-% date_settlement : scalar [1x1] : settlement date
+% Computation of the Discount Factor after the USA spread
+% 
+% INPUT:
+% B_bar :               SCALAR [1x1] : discount factor at time 0
+% data :                STRUCT : market data
+% date_settlement :     SCALAR [1x1] : settlement date
+% 
 % OUTPUT
 % discount : vector [1xN] : discount factor
 %
 % USES : discount_factor(B_bar , data , date_settlement)
 
-dates = datenum(data.datesExpiry);
+    %% Conventions
+    conv_ACT365 = 3;
 
-dt = yearfrac(date_settlement ,   dates(1:end));
-
-% mean spread
-spread = 34*1e-4;
-
-% Compute the discount factor
-
-discount = B_bar .* exp( spread .* dt);
-
+    %% Computation of the dates
+    dates = datenum(data.datesExpiry);
+    dt = yearfrac(date_settlement ,   dates(1:end), conv_ACT365);
+    
+    % mean spread
+    spread = 34*1e-4;
+    
+    %% Computation of the discount factor
+    discount = B_bar .* exp( spread .* dt);
 
 end % function discount_factor
