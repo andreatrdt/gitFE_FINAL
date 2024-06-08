95b31977cdc9a1b16953a9dd337e9402b664cf52
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-06-08 13:03:53 +0200

    Update disp_contract_prices.m

--- Project2_MultivariatePricing/plotting/disp_contract_prices.m
+++ Project2_MultivariatePricing/plotting/disp_contract_prices.m
@@ -19,15 +19,15 @@ function disp_contract_prices(price_Levy,CI_Levy,price_Blk,CI_Blk,price_Blk_AV,C
 
     disp('Prices of the derivative')
     disp('-----------------------------------------------------------------------------------------------------')
-    fprintf(' Model              |  Price               |  Confidence Interval \n')
+    fprintf(' Model              |  Price              |  Confidence Interval \n')
     disp('-----------------------------------------------------------------------------------------------------')
-    fprintf('Levy:               |  %.8f%%        |  [ %.8f%% ,  %.8f%%] \n', price_Levy, CI_Levy(1), CI_Levy(2))
+    fprintf('Levy:               |  %.8f        |  [ %.8f ,  %.8f] \n', price_Levy, CI_Levy(1), CI_Levy(2))
     
-    fprintf('Black:              |  %.8f%%        |  [ %.8f%% ,  %.8f%%] \n', price_Blk, CI_Blk(1), CI_Blk(2))
+    fprintf('Black:              |  %.8f        |  [ %.8f ,  %.8f] \n', price_Blk, CI_Blk(1), CI_Blk(2))
     
-    fprintf('Black AV:           |  %.8f%%        |  [ %.8f%% ,  %.8f%%] \n', price_Blk_AV, CI_Blk_AV(1), CI_Blk_AV(2))
+    fprintf('Black AV:           |  %.8f        |  [ %.8f ,  %.8f] \n', price_Blk_AV, CI_Blk_AV(1), CI_Blk_AV(2))
     
-    fprintf('Black semi-closed:  |  %.8f%%        |  -- \n', price_SemiclosedBlk)
+    fprintf('Black semi-closed:  |  %.8f        |  -- \n', price_SemiclosedBlk)
 
     disp('-----------------------------------------------------------------------------------------------------')    
 
