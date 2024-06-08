ae66df61a9f9048bad055d40d0dedf74205f0f78
MarcoMaspes <marco.maspes00@gmail.com>
2024-06-08 16:16:00 +0200

    AV changes

--- Project2_MultivariatePricing/pricing_certificate/stock_simulation_Levy.m
+++ Project2_MultivariatePricing/pricing_certificate/stock_simulation_Levy.m
@@ -89,9 +89,9 @@ function [stock , stock_AV] = stock_simulation_Levy(idiosync_USA, idiosync_EU, s
     Y_1_AV = [Y_1(1:nSim/2, :); ...
         -(0.5+Beta_USA) * gamma_USA^2 .*G_1(nSim/2 + 1:end, :) * TTM - gamma_USA .* sqrt(TTM .* G_1(nSim/2 + 1:end, :)) .* g_1(nSim/2 + 1:end, :)];
     Y_2_AV = [Y_2(1:nSim/2, :); ...
-        -(0.5+Beta_EU) * gamma_EU^2 .* G_2(nSim/2 + 1:end, :) * TTM + gamma_EU .* sqrt(TTM .* G_2(nSim/2 + 1:end, :)) .* g_2(nSim/2 + 1:end, :)];
+        -(0.5+Beta_EU) * gamma_EU^2 .* G_2(nSim/2 + 1:end, :) * TTM - gamma_EU .* sqrt(TTM .* G_2(nSim/2 + 1:end, :)) .* g_2(nSim/2 + 1:end, :)];
     Z_AV = [Z(1:nSim/2, :); ...
-        -(0.5+Beta_z) * gamma_z^2 .* G_z(nSim/2 + 1:end, :) * TTM + gamma_z .* sqrt(TTM .* G_z(nSim/2 + 1:end, :)) .* g_z(nSim/2 + 1:end, :)];
+        -(0.5+Beta_z) * gamma_z^2 .* G_z(nSim/2 + 1:end, :) * TTM - gamma_z .* sqrt(TTM .* G_z(nSim/2 + 1:end, :)) .* g_z(nSim/2 + 1:end, :)];
 
     % Marginal processes
     X_1_AV = Y_1_AV + a_USA * Z_AV;
